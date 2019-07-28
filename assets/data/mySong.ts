ba0ef708bf5b4a263929fe0739a0cd0f0089ad64
JJJason321 <52867885+JJJason321@users.noreply.github.com>
2019-07-28 01:26:56 -0400

    Add files via upload
    here is the assets folder

---
+++ assets/data/mySong.ts
@@ -0,0 +1,33 @@
+import {Song} from "../../app/song";
+
+export const mySongs: Song [] = 
+[
+    {
+        name:"Believer",
+        artist:"Imagine Dragrons",
+        genre:"Rock",
+        yearReleased:"2015",
+        picture:"../../assets/pic/Believer.jpg"
+    },
+    {
+        name:"YSIV",
+        artist:"Logic",
+        genre:"Rap",
+        yearReleased:"2017",
+        picture:"../../assets/pic/ysiv.jpg"
+    },
+    {
+        name:"People Get Up and Drive Your Funky Soul",
+        artist:"James Brown",
+        genre:"Funk",
+        yearReleased:"1986",
+        picture:"../../assets/pic/soul.jpg"
+    },
+    {
+        name:"Equniox",
+        artist:"John Coltrane",
+        genre:"Jazz",
+        yearReleased:"1966",
+        picture:"../../assets/pic/equnix.jpg"
+    }
+]
